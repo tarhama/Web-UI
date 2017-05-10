68f03da957e448e8b637ecfcdae80f18a4738f55
Marek Talpas <marek.talpas@gmail.com>
2017-05-10 16:09:35 +0200

    creating directories for js parts(components)

---
+++ js_parts/navbar/language_dropdown.js
@@ -0,0 +1,31 @@
+var LanguageDropdown = (function(nav) {
+
+  // cahing DOM
+  var $icons = $('.language, .arrow-drop-down');
+  var $dropdownWrapper = $('.dropdown-content');
+  var $dropDown = $('.dropdown-content, .dropdown-content__chevron');
+
+  // variables definitions
+  var dropdownString ='';
+  var dropdownStuff = '.language,.arrow-drop-down,.dropdown-content,.dropdown-content__chevron';
+  var LanguagesArray = nav.languages;
+
+  // rendering DOM
+  LanguagesArray.forEach(function(currLang, index, all) {
+    dropdownString += '<a href="">' + currLang.language + '</a>';
+  });
+
+  $dropdownWrapper.html(dropdownString);
+
+  // event handlers
+  $icons.on('click', function() {
+    $dropDown.slideToggle();
+  });
+
+  $('body').click(function(e){
+    if ( !$(e.target).is(dropdownStuff)) {
+      $dropDown.slideUp();
+    }
+  });
+
+})(NavbarData);
