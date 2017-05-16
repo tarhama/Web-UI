adc62f99f68b441933c4cdb5cce3dffe70cd315d
Matus <bernatmatus@gmail.com>
2017-05-16 10:22:31 +0200

    add section7, section2 pure js, update html

---
+++ data/section7.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//TITLE
+var FeaturesSec7 = (function () {
+  return {
+    titleSec7: 'Apps to connect your world',
+    headingSec7: "Choose from over 200high-quality apps that make it " +
+                  "simple to add advanced features like appointment " +
+                  "scheduling, accounting, live chat and more.",
+    pictures: [
+      { id: 0,
+        url: 'assets/images/bottom.jpg',
+        alt: 'first',
+        class: 'home-apps__item-bottom',
+      },
+      { url: 'assets/images/mid.png',
+        alt: 'second',
+        class: 'home-apps__item-mid',
+        id: 1 
+      },
+      { url: 'assets/images/top.png',
+        alt: 'third',
+        class: 'home-apps__item-top',
+        id: 2
+      },
+      { url: 'assets/images/featured.png',
+        alt: 'fourth',
+        class: 'home-apps__item-featured',
+        id: 3
+      },
+    ],            
+  };
+
+})();
