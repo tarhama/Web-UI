af7c0949035540cefe3501721a5a83f69adeb316
kamil jancar <kamiljancar@gmail.com>
2017-05-17 08:54:17 +0200

    splitted JS for sections 3,4,6

---
+++ data/section3.js
@@ -0,0 +1,36 @@
+var section3_data = {
+    "title": "Sell more with powerful eCommerce",
+    "checklistTexts": [
+        "<strong>Beautiful design</strong> to customize your storefront and showcase your products",
+        "<strong>Complete platform</strong> to manage inventory, track orders and handle taxes",
+        "<strong>Advanced tools</strong>: real time shipping, abandoned cart, coupons and currencies"
+    ],
+    "buttons": [
+        {
+            "btn_text": "GET STARTED",
+            "btn_link_text": "LEARN MORE"
+        }
+    ],
+    "images" : [
+        {
+            "src": "./assets/images/box.jpg",
+            "name": "section3_box",
+            "className": " item-box"
+        },
+        {
+            "src": "./assets/images/orders.png",
+            "name": "section3_orders",
+            "className": " item-orders"
+        },
+        {
+            "src": "./assets/images/iphone.png",
+            "name": "section3_iphone",
+            "className": " item-iphone"
+        },
+        {
+            "src": "./assets/images/monthly-sales.png",
+            "name": "section3_monthly_sales",
+            "className": " item-sales"
+        }
+    ]
+};
