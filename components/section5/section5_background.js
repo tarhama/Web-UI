c51f500704210dabd9a5ea791090749081416894
Matus <bernatmatus@gmail.com>
2017-05-17 16:35:48 +0200

    delete unnecessary file, update section5_background.js

--- components/section5/section5_background.js
+++ components/section5/section5_background.js
@@ -2,20 +2,6 @@
 
 $(function () {
 
-  $(window).scroll(function () {
-    var height = $(window).scrollTop();
-    
-    //AFTER LOAD OR REFRESH PAGE AND SCROLLING TO THIS SECTION
-    /* SECTION 5 ANIMATE */
-    if (height > 3800) {
-        $('.section5 .section5-text.opacity').addClass('is-active');
-        $('.section5 .section5-title.opacity').addClass('tabs-transition');
-        $('.section5 .state-btn.opacity').addClass('btn-active');
-        $('.section5 .app-icon.opacity').addClass('app-active');
-    }
-    /* END OF SECTION 5 ANIMATE */
-});
-
   var PhonesSec5 = (function (val) {
 
     var backgPhones = val.phones;
@@ -36,42 +22,62 @@ $(function () {
 
     $(".section5 .section5-container").append(phoneFrag);
   })(FeaturesSec5);
-});
 
-$(function(){
-  $(window).on('scroll', function() {
-    var body = document.body,
-    html = document.documentElement;
-    var height = Math.max( body.scrollHeight, body.offsetHeight,
-     html.clientHeight, html.scrollHeight, html.offsetHeight );
 
-    height = height - $(window).height();
+  var scrollFunction = (function (val) {
 
-    var top = $(window).scrollTop();
+    var iphone = val.phones[0].class;
+    var iphone2 = val.phones[1].class;
+    var android = val.phones[2].class;
+    var nexus = val.phones[3].class;
 
-    var scroll = (height - top)/(height);
 
+    $(window).on('scroll', function() {
+      var body = document.body,
+      html = document.documentElement;
+      var height = Math.max( body.scrollHeight, body.offsetHeight,
+      html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+      height = height - $(window).height();
+
+      var top = $(window).scrollTop();
+      var scroll = (height - top)/(height);
+
+    //AFTER LOAD OR REFRESH PAGE AND SCROLLING TO THIS SECTION
+    /* SECTION 5 ANIMATE */
+    if (top > 3800) {
+      $('.section5 .section5-text.opacity').addClass('is-active');
+      $('.section5 .section5-title.opacity').addClass('tabs-transition');
+      $('.section5 .state-btn.opacity').addClass('btn-active');
+      $('.section5 .app-icon.opacity').addClass('app-active');
+    }
+    /* END OF SECTION 5 ANIMATE */
+
+    //SECTION 7
     $(".mid").css({top: (scroll)*220 + 'px', transitionDuration: '0.6s' });
     $(".top").css({top: (scroll)*150 + 'px', transitionDuration: '0.6s' });
     $(".featured").css({top: (scroll)*320 + 'px', transitionDuration: '0.6s' });
+    //END OF SECTION 7
 
     //SECTION 5 ANIMATION WHEN SCROLLING
-    $(".mobile-iphone").css({
+    $("." + iphone).css({
       'transform': 'translateY(' + (scroll - 0.4) *200 + 'vh) rotate('+ (scroll - 1) *-30 + 'deg)',
       'transitionDuration': '0.6s'
     });
-    $(".mobile-iphone02").css({
+    $("." + iphone2).css({
       'transform': 'translateY(' + (scroll - 0.1) *120 + 'vh) rotate('+ scroll *-15 + 'deg) ',
       'transitionDuration': '0.6s'
     });
-    $(".mobile-android").css({
+    $("." + android).css({
       'transform': 'translateY(' + (scroll - 0.2) *150 + 'vh) rotate('+ scroll *-15 + 'deg)',
       'transitionDuration': '0.6s'
     });
-    $(".mobile-nexus").css({
+    $("." + nexus).css({
       'transform': 'translateY(' + (scroll - 0.7) *70 + 'vh) rotate('+ (scroll - 0.5) *-25 + 'deg)',
       'transitionDuration': '0.6s'
     });
 
   }).trigger('scroll');
+
+  })(FeaturesSec5);
 });
