4bcbe2f9d6061517f1ec05e54ce24248089e000d
dominik <dmnk.kolesar@gmail.com>
2017-05-17 22:38:51 +0200

    section 1

---
+++ js_parts/section1/section1-data.js
@@ -0,0 +1,10 @@
+
+var configForSection1 = {
+    "title": "hello all",
+    "videoAttributes": {
+            "videoSrc":"http://cdn2.editmysite.com/videos/landing-pages/global/home/masthead/masthead-720.mp4",
+            "videoType": "video/mp4"
+        }
+
+};
+
---
+++ js_parts/section1/section1Scripts.js
@@ -0,0 +1,52 @@
+var Section1Processor = function () {
+
+    var data;
+    var counter = 0;
+    var animatedText;
+    var splitedText;
+    var videoElem;
+    var publicMethods = {};
+    var videoSrc;
+    var videoType;
+
+    publicMethods.init = function (configForSection1) {
+        animatedText = document.getElementById("text-animation");
+        videoElem = document.getElementById("section1_video");
+        console.log("init");
+        data = configForSection1;
+        videoSrc = configForSection1.videoAttributes.videoSrc;
+        videoType = configForSection1.videoAttributes.videoType;
+        splitedText= configForSection1.title.split("");
+    };
+
+    publicMethods.addVideoSrc = function () {
+        var source = document.createElement('source');
+        source.src = videoSrc;
+        source.type = videoType;
+        videoElem.appendChild(source);
+    };
+
+    publicMethods.animateText = function () {
+        setInterval(appendToTheRight, 200);
+    };
+    appendToTheRight = function () {
+        animatedText.innerHTML += splitedText[counter];
+        counter++;
+        if (counter > splitedText.length) {
+            counter = 0;
+            animatedText.innerHTML = "";
+        }
+    };
+    return publicMethods;
+}();
+
+window.onload= function () {
+    Section1Processor.init(configForSection1);
+    Section1Processor.animateText();
+    Section1Processor.addVideoSrc();
+};
+
+
+
+
+
