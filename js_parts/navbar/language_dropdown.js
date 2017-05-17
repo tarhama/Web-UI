5740852c069fd691a75800763000ec24be889730
Marek Talpas <marek.talpas@gmail.com>
2017-05-17 13:47:05 +0200

    renamed classes

--- js_parts/navbar/language_dropdown.js
+++ js_parts/navbar/language_dropdown.js
@@ -3,14 +3,14 @@
 var LanguageDropdown = (function(nav) {
 
   // cahing DOM
-  var $icons = $('.language, .arrow-drop-down');
-  var $dropdownWrapper = $('.dropdown-content');
-  var $chevronTop = $('.dropdown-content__chevron');
-  var $dropDown = $('.dropdown-content, .dropdown-content__chevron');
+  var $icons = $('.header .navbar .list-right .language, .header .navbar .list-right .arrow-drop-down');
+  var $dropdownWrapper = $('.header .navbar .list-right .dropdown-content');
+  var $chevronTop = $('.header .navbar .list-right .chevron-up');
+  var $dropDown = $('.header .navbar .list-right .dropdown-content, .header .navbar .list-right .chevron-up');
 
   // variables definitions
   var dropdownString ='';
-  var dropdownStuff = '.language,.arrow-drop-down,.dropdown-content,.dropdown-content__chevron';
+  var dropdownStuff = '.header .navbar .list-right .language, .header .navbar .list-right .arrow-drop-down, .header .navbar .list-right .dropdown-content, .header .navbar .list-right .chevron-up';
   var LanguagesArray = nav.languages;
 
   // rendering DOM
@@ -24,6 +24,7 @@ var LanguageDropdown = (function(nav) {
   $icons.on('click', function() {
     if ($chevronTop.hasClass('rolled_down')) {
       $dropDown.slideUp();
+      $chevronTop.removeClass('rolled_down');
     } else {
       $chevronTop.addClass('rolled_down');
       $dropDown.slideDown();
