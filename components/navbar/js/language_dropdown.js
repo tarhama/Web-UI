73d79f50a93f28279482ea4dce8d4226fde515d9
Marek Talpas <marek.talpas@gmail.com>
2017-05-24 13:05:37 +0200

    added bootstrap cdn links

--- components/navbar/js/language_dropdown.js
+++ components/navbar/js/language_dropdown.js
@@ -1,41 +1,41 @@
-'use strict';
+// 'use strict';
 
-var LanguageDropdown = (function(nav) {
+// var LanguageDropdown = (function(nav) {
 
-  // cahing DOM
-  var $icons = $('.header .navbar .list-right .language, .header .navbar .list-right .arrow-drop-down');
-  var $dropdownWrapper = $('.header .navbar .list-right .dropdown-content');
-  var $chevronTop = $('.header .navbar .list-right .chevron-up');
-  var $dropDown = $('.header .navbar .list-right .dropdown-content, .header .navbar .list-right .chevron-up');
+//   // cahing DOM
+//   var $icons = $('.header .navbar .list-right .language, .header .navbar .list-right .arrow-drop-down');
+//   var $dropdownWrapper = $('.header .navbar .list-right .dropdown-content');
+//   var $chevronTop = $('.header .navbar .list-right .chevron-up');
+//   var $dropDown = $('.header .navbar .list-right .dropdown-content, .header .navbar .list-right .chevron-up');
 
-  // variables definitions
-  var dropdownString ='';
-  var dropdownStuff = '.header .navbar .list-right .language, .header .navbar .list-right .arrow-drop-down, .header .navbar .list-right .dropdown-content, .header .navbar .list-right .chevron-up';
-  var LanguagesArray = nav.languages;
+//   // variables definitions
+//   var dropdownString ='';
+//   var dropdownStuff = '.header .navbar .list-right .language, .header .navbar .list-right .arrow-drop-down, .header .navbar .list-right .dropdown-content, .header .navbar .list-right .chevron-up';
+//   var LanguagesArray = nav.languages;
 
-  // rendering DOM
-  LanguagesArray.forEach(function(language) {
-    dropdownString += '<a href="">' + language.language + '</a>';
-  });
+//   // rendering DOM
+//   LanguagesArray.forEach(function(language) {
+//     dropdownString += '<a href="">' + language.language + '</a>';
+//   });
 
-  $dropdownWrapper.html(dropdownString);
+//   $dropdownWrapper.html(dropdownString);
 
-  // event handlers
-  $icons.on('click', function() {
-    if ($chevronTop.hasClass('rolled_down')) {
-      $dropDown.slideUp();
-      $chevronTop.removeClass('rolled_down');
-    } else {
-      $chevronTop.addClass('rolled_down');
-      $dropDown.slideDown();
-    }
-  });
+//   // event handlers
+//   $icons.on('click', function() {
+//     if ($chevronTop.hasClass('rolled_down')) {
+//       $dropDown.slideUp();
+//       $chevronTop.removeClass('rolled_down');
+//     } else {
+//       $chevronTop.addClass('rolled_down');
+//       $dropDown.slideDown();
+//     }
+//   });
 
-  $('body').click(function(e) {
-    if ( !$(e.target).is(dropdownStuff)) {
-      $dropDown.slideUp();
-      $chevronTop.removeClass('rolled_down');
-    }
-  });
+//   $('body').click(function(e) {
+//     if ( !$(e.target).is(dropdownStuff)) {
+//       $dropDown.slideUp();
+//       $chevronTop.removeClass('rolled_down');
+//     }
+//   });
 
-})(NavbarData);
+// })(NavbarData);
--- components/navbar/js/navbar_roller.js
+++ components/navbar/js/navbar_roller.js
@@ -1,116 +1,116 @@
-'use strict';
+// 'use strict';
 
-var NavbarRoller = (function() {
+// var NavbarRoller = (function() {
 
-  // variables definitions
-  var scrollStart = 0;
-  var offsetTop = 86; //navbar height + border
-  var pureWhite = '#fff';
-  var almostPureWhite = 'rgba(255,255,255,0.8)';
-  var darkishGrey = '#666c70';
-  var lightishGrey = '#9ba0a3';
-  var darkestGrey = '#363b3e';
-  var navyBlue = '#2990ea';
-  var weeblyBlueUrl = 'assets/images/weebly-logo-blue.svg';
-  var weeblyWhiteUrl = 'assets/images/weebly-logo-white.svg';
+//   // variables definitions
+//   var scrollStart = 0;
+//   var offsetTop = 86; //navbar height + border
+//   var pureWhite = '#fff';
+//   var almostPureWhite = 'rgba(255,255,255,0.8)';
+//   var darkishGrey = '#666c70';
+//   var lightishGrey = '#9ba0a3';
+//   var darkestGrey = '#363b3e';
+//   var navyBlue = '#2990ea';
+//   var weeblyBlueUrl = 'assets/images/weebly-logo-blue.svg';
+//   var weeblyWhiteUrl = 'assets/images/weebly-logo-white.svg';
 
-  // chaching DOM
-  var $navbar = $('.navbar');
-  var $weeblyLogo = $('.header .navbar .weebly-logo');
-  var $navbarTextItems = $('.header .navbar .list-left > li > a, .header .navbar .list .login > a');
-  var $languageIcon = $('.header .navbar .list-right .language');
-  var $arrowDown = $('.header .navbar .list-right .arrow-drop-down');
-  var $signupBtn = $('.header .navbar .list-right .signup-btn');
+//   // chaching DOM
+//   var $navbar = $('.navbar');
+//   var $weeblyLogo = $('.header .navbar .weebly-logo');
+//   var $navbarTextItems = $('.header .navbar .list-left > li > a, .header .navbar .list .login > a');
+//   var $languageIcon = $('.header .navbar .list-right .language');
+//   var $arrowDown = $('.header .navbar .list-right .arrow-drop-down');
+//   var $signupBtn = $('.header .navbar .list-right .signup-btn');
 
-  $( document ).ready(function() {
-    scrollStart = $(this).scrollTop();
-    if(scrollStart > offsetTop) {
-      $navbar.slideDown('slow', depictWhiteNavbar);
-    } else {
-      depictTransparentNavbar();
-    }
-  });
+//   $( document ).ready(function() {
+//     scrollStart = $(this).scrollTop();
+//     if(scrollStart > offsetTop) {
+//       $navbar.slideDown('slow', depictWhiteNavbar);
+//     } else {
+//       depictTransparentNavbar();
+//     }
+//   });
 
-  function depictWhiteNavbar() {
-    $navbar.css('background-color', pureWhite);
-    $weeblyLogo.attr('src', weeblyBlueUrl);
-    $navbarTextItems.css('color', darkishGrey);
-    $languageIcon.css('color', darkishGrey);
-    $arrowDown.css('color', darkishGrey);
-    $signupBtn.css({'color': '#2990ea',
-      'border': '1px solid #666c70'
-    });
+//   function depictWhiteNavbar() {
+//     $navbar.css('background-color', pureWhite);
+//     $weeblyLogo.attr('src', weeblyBlueUrl);
+//     $navbarTextItems.css('color', darkishGrey);
+//     $languageIcon.css('color', darkishGrey);
+//     $arrowDown.css('color', darkishGrey);
+//     $signupBtn.css({'color': '#2990ea',
+//       'border': '1px solid #666c70'
+//     });
 
-    $navbarTextItems.hover(
-      function() {
-        $(this).css('color', darkestGrey);
-      }, function() {
-        $(this).css('color', darkishGrey);
-      });
-    $languageIcon.hover(
-      function() {
-        $(this).css('color', darkestGrey);
-      }, function() {
-        $(this).css('color', darkishGrey);
-      });
-    $arrowDown.hover(
-      function() {
-        $(this).css('color', darkestGrey);
-      }, function() {
-        $(this).css('color', darkishGrey);
-      });
-    $signupBtn.hover(
-      function() {
-        $(this).css('box-shadow', 'inset 0 0 5px #666c70');
-      }, function() {
-        $(this).css({ 'border': '1px solid #666c70', 'box-shadow': 'none' });
-      });
-  }
+//     $navbarTextItems.hover(
+//       function() {
+//         $(this).css('color', darkestGrey);
+//       }, function() {
+//         $(this).css('color', darkishGrey);
+//       });
+//     $languageIcon.hover(
+//       function() {
+//         $(this).css('color', darkestGrey);
+//       }, function() {
+//         $(this).css('color', darkishGrey);
+//       });
+//     $arrowDown.hover(
+//       function() {
+//         $(this).css('color', darkestGrey);
+//       }, function() {
+//         $(this).css('color', darkishGrey);
+//       });
+//     $signupBtn.hover(
+//       function() {
+//         $(this).css('box-shadow', 'inset 0 0 5px #666c70');
+//       }, function() {
+//         $(this).css({ 'border': '1px solid #666c70', 'box-shadow': 'none' });
+//       });
+//   }
 
-  function depictTransparentNavbar() {
-    $navbar.css('background-color', 'transparent');
-    $weeblyLogo.attr('src', weeblyWhiteUrl);
-    $navbarTextItems.css('color', almostPureWhite);
-    $languageIcon.css('color', almostPureWhite);
-    $arrowDown.css('color', almostPureWhite);
-    $signupBtn.css({'color': almostPureWhite,
-      'border': '2px solid #fff',
-      'box-shadow': 'none'
-    });
+//   function depictTransparentNavbar() {
+//     $navbar.css('background-color', 'transparent');
+//     $weeblyLogo.attr('src', weeblyWhiteUrl);
+//     $navbarTextItems.css('color', almostPureWhite);
+//     $languageIcon.css('color', almostPureWhite);
+//     $arrowDown.css('color', almostPureWhite);
+//     $signupBtn.css({'color': almostPureWhite,
+//       'border': '2px solid #fff',
+//       'box-shadow': 'none'
+//     });
 
-    $navbarTextItems.hover(
-      function() {
-        $(this).css('color', pureWhite);
-      }, function() {
-        $(this).css('color', almostPureWhite);
-      });
-    $languageIcon.hover(
-      function() {
-        $(this).css('color', pureWhite);
-      }, function() {
-        $(this).css('color', almostPureWhite);
-      });
-    $arrowDown.hover(
-      function() {
-        $(this).css('color', pureWhite);
-      }, function() {
-        $(this).css('color', almostPureWhite);
-      });
-    $signupBtn.hover(
-      function() {
-        $(this).css('box-shadow', 'inset 0 0 5px #fff');
-      }, function() {
-        $(this).css('border', '2px solid rgba(255,255,255,0.8)');
-      });
-  }
+//     $navbarTextItems.hover(
+//       function() {
+//         $(this).css('color', pureWhite);
+//       }, function() {
+//         $(this).css('color', almostPureWhite);
+//       });
+//     $languageIcon.hover(
+//       function() {
+//         $(this).css('color', pureWhite);
+//       }, function() {
+//         $(this).css('color', almostPureWhite);
+//       });
+//     $arrowDown.hover(
+//       function() {
+//         $(this).css('color', pureWhite);
+//       }, function() {
+//         $(this).css('color', almostPureWhite);
+//       });
+//     $signupBtn.hover(
+//       function() {
+//         $(this).css('box-shadow', 'inset 0 0 5px #fff');
+//       }, function() {
+//         $(this).css('border', '2px solid rgba(255,255,255,0.8)');
+//       });
+//   }
 
-  $(document).scroll(function() {
-    scrollStart = $(this).scrollTop();
-    if(scrollStart > offsetTop) {
-      $navbar.slideDown('slow', depictWhiteNavbar);
-    } else {
-      depictTransparentNavbar();
-    }
-  });
+//   $(document).scroll(function() {
+//     scrollStart = $(this).scrollTop();
+//     if(scrollStart > offsetTop) {
+//       $navbar.slideDown('slow', depictWhiteNavbar);
+//     } else {
+//       depictTransparentNavbar();
+//     }
+//   });
 
-})();
+// })();
