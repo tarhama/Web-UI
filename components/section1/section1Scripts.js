ca2a41483b9222aa78a5a207fdd1ffe694cb5cee
dominik <dmnk.kolesar@gmail.com>
2017-05-30 15:45:55 +0200

    animated text

--- components/section1/section1Scripts.js
+++ components/section1/section1Scripts.js
@@ -2,22 +2,21 @@ var Section1Processor = function () {
 
     var data;
     var counter = 0;
-    //var animatedText;
-    //var splitedText;
+    var animatedText;
+    var splitedText;
     var videoElem;
     var publicMethods = {};
     var videoSrc;
     var videoType;
 
     publicMethods.init = function (configForSection1) {
-        //animatedText = document.getElementById("text-animation");
+        animatedText = document.getElementById("text-animation");
         videoElem = document.getElementById("section1_video");
         console.log("init");
         data = configForSection1;
         videoSrc = configForSection1.videoAttributes.videoSrc;
         videoType = configForSection1.videoAttributes.videoType;
-        videoType = configForSection1.videoAttributes.videoType;
-        //splitedText= configForSection1.title.split("");
+        splitedText= configForSection1.title.split("");
     };
 
     publicMethods.addVideoSrc = function () {
@@ -27,17 +26,18 @@ var Section1Processor = function () {
         videoElem.appendChild(source);
     };
 
-    // publicMethods.animateText = function () {
-    //     setInterval(appendToTheRight, 200);
-    // };
-    // appendToTheRight = function () {
-    //     animatedText.innerHTML += splitedText[counter];
-    //     counter++;
-    //     if (counter > splitedText.length) {
-    //         counter = 0;
-    //         animatedText.innerHTML = "";
-    //     }
-    // };
+     publicMethods.animateText = function () {
+         setInterval(publicMethods.appendToTheRight, 200);
+     };
+
+    publicMethods.appendToTheRight = function () {
+        animatedText.innerHTML += splitedText[counter];
+        counter++;
+        if (counter > splitedText.length) {
+            counter = 0;
+            animatedText.innerHTML = "";
+        }
+    };
     return publicMethods;
 }();
 
