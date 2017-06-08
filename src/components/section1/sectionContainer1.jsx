def830e23ae4871945b443f3e47f89e50b9a816a
dominik <dmnk.kolesar@gmail.com>
2017-06-08 09:27:16 +0200

    animated text

--- src/components/section1/sectionContainer1.jsx
+++ src/components/section1/sectionContainer1.jsx
@@ -3,10 +3,48 @@ import './sectionContainer1.css';
 
 
 export default class SectionContainer1 extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			counter:0,
+			textToShow:"",
+			splitedText:[],
+			originText:"DO WHAT YOU WANT TO DO"
+		};
+		this.setSplitedText = this.setSplitedText.bind(this);
+		this.appendLetterToTheRight = this.appendLetterToTheRight.bind(this);
+	}
+	componentWillMount(){
+		this.setSplitedText();
+	}
+	componentDidMount(){
+		setInterval(this.appendLetterToTheRight,200);
+	}
+
+	appendLetterToTheRight(){
+		if (this.state.counter < this.state.splitedText.length) {
+			this.setState({
+				textToShow: this.state.textToShow + this.state.splitedText[this.state.counter],
+				counter: this.state.counter + 1,
+			},() => {
+				console.log(this.state.textToShow);
+			});
+		}
+	}
+	setSplitedText(){
+		this.setState({
+			splitedText: this.state.originText.split("")
+		}, () => {
+			console.log(this.state.splitedText);
+		});
+	}
 	render() {
 		return(
 			<div className="sectionContainer1">
 				<div className="sectionContainer1_overlay">
+					<div>
+						{this.state.textToShow}
+					</div>
 				</div>
 				<video className="sectionContainer1_video" autoPlay loop muted>
 					<source
