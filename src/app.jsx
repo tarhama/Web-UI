aef849dac9da4048957ed3eb9657716cfb858d5c
lubos.jasko <lubos.jasko@globallogic.com>
2017-06-09 10:26:43 +0200

    Section 5 Done

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../styles/index.scss';
+// import '../styles/bootstrap.min.css'
+// import {Bootstrap} from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import SectionContainer1 from './components/section1/sectionContainer1';
+import {Section5Container} from "./components/section5/section5Container";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
 				<SectionContainer1 />
+				<Section5Container/>
       </div>
     )
   }
---
+++ src/components/section5/parts/sectionContent5.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const SectionContent5 = ({
+													 titleSection5,
+													 headingSection5,
+													 btnSection5,
+													 apple: {src: src1, alt: alt1},
+													 google: {src:src2, alt:alt2}}
+													 ) => (
+
+	<div className="landing-page-content">
+		<h2 className="h1 section5-title opacity">{titleSection5}</h2>
+		<p className="section5-text opacity">{headingSection5}</p>
+		<div className="state-btn opacity">
+			<a className="raise-btn">{btnSection5}</a>
+		</div>
+		<div className="app-icon opacity">
+			<img src={src1}
+					 alt={alt1}/>
+			<img src={src2}
+					 alt={alt2}/>
+		</div>
+	</div>
+);
+
+export default SectionContent5;
+
+
+
+
+
+
