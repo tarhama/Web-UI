152e0fee7f9d713ffa8d2bdb54671c7af86c8772
SYNAPSE\matus.bernat <matus.bernat@globallogic.com>
2017-06-09 12:21:36 +0200

    add dropdown

---
+++ src/components/navbar/navbarRight/languages.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+const Languages = [
+  {
+    id: 1,
+    name: 'chinese',
+  },
+  {
+    id: 2,
+    name: 'dansk',
+  },
+  {
+    id: 3,
+    name: 'english',
+  },
+  {
+    id: 4,
+    name: 'italy',
+  },
+  {
+    id: 5,
+    name: 'german',
+  },
+  {
+    id: 6,
+    name: 'polish',
+  },
+  {
+    id: 7,
+    name: 'russian',
+  },
+  {
+    id: 8,
+    name: 'slovak',
+  }
+];
+
+export default Languages;
