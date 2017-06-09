9cb0d095c0a4164c7cd98bc84d734dd653f1ce60
lubos.jasko <lubos.jasko@globallogic.com>
2017-06-09 12:44:25 +0200

    added bootstrap4 to package.json

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import SectionContainer1 from './components/section1/sectionContainer1';
 import {Section5Container} from "./components/section5/section5Container";
 import NavbarComponent from './components/navbar/navbarComponent';
 import Section2Container from './components/section2/section2_container';
+import Section7Container from './components/Section7/section7Container';
 
 export default class App extends React.Component {
 	render() {
@@ -14,6 +15,7 @@ export default class App extends React.Component {
 				<SectionContainer1 />
 				<Section2Container />
 				<Section5Container />
+				<Section7Container />
 			</div>
 		)
 	}
---
+++ src/components/Section7/section7Container.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './section7Container.css'
+
+export default class Section7Container extends React.Component {
+
+	constructor() {
+		super();
+	}
+
+	render() {
+		return(
+			<div>Section7 work!!</div>
+		)
+	}
+}
+
