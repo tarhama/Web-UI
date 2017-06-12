cfbd9783dbf9d053ae74076a5efa61e74c4f8451
Matus <bernatmatus@gmail.com>
2017-06-12 22:28:51 +0200

    fill section9

--- src/components/section9/leftColumn/leftColumnSec9.jsx
+++ src/components/section9/leftColumn/leftColumnSec9.jsx
@@ -4,17 +4,33 @@ import './leftColumnSec9.scss';
 
 class LeftColumnSec9 extends Component {
   render() {
-    //const title = data.column.find(col => col.id === 1);
+    const title = data.DataSect9.columns.find(col => col.id === 1).title;
+    const list = data.DataSect9.columns.find(col => col.id === 1).list;
+    
+    const listUl = list.map((name, index) => {
+      return this.myList(name, index)
+    })
+    
     const logo = require('../../../../assets/images/circle-check.svg');
-    //console.log(data.DataSect9.listImg.url);
+    
     return(
-      <div>
-        <img src={logo} />
+      <div className="column">
+        <div className="columnHeader">
+          <img src={logo} />
+          <h3 className="titleSec9">{title}</h3>
+        </div>
+        <div className="listContent">
+          {listUl}
+        </div>
       </div>
     )
   }
 
-
+  myList(name, index) {
+    return(
+      <li key={index}>{name.name}</li>
+    )
+  }
 }
 
 export default LeftColumnSec9;
