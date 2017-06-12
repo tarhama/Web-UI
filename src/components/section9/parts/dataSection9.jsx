8142143f12c5782fc4e5f9165a8308388c8fdeb8
Matus <bernatmatus@gmail.com>
2017-06-12 13:08:36 +0200

    add datasection9

--- src/components/section9/parts/dataSection9.jsx
+++ src/components/section9/parts/dataSection9.jsx
@@ -1,41 +1,57 @@
 
-const Data = {
-  "Title": "Features to start, sell & grow",
-  "ListImg": "./assets/images/circle-check.svg",
-  "LeftColumn": 
-    [
-      "Website",
-      "Simple drag & drop editor",
-      "Step-by-step guidance",
-      "Pre-designed layout options",
-      "Domain names & professional email",
-      "Video backgrounds",
-      "Site stats",
-      "Create a blog"
-    ],
-  "MiddleColumn": 
-    [
-      "eCommerce",
-       "Unlimited products",
-       "Optimized one-page checkout",
-       "Automatic tax calculator",
-       "Real time shipping rates",
-       "Abandoned cart recovery",
-       "Coupon codes",
-       "Digital gift cards"
-    ],
-    "RightColumn":
-    [
-      "Marketing",
-      "Fully integrated with site & store",
-      "Suggested emails created automatically",
-      "Email to sales insights",
-      "Branded billing emails",
-      "Powerful app integrations",
-      "SEO Guide & scorecard",
-      "Automation for marketing autopilot"
-    ],
+const Data = [
+  {
+    title: "Features to start, sell & grow",
+  },
+  {
+    listImg: "./assets/images/circle-check.svg",
+  },
+  {
     "Button": "see all features",
-};
+  },
+  {
+    columns: [
+    {
+      id:1,
+      list: [
+        "Website",
+        "Simple drag & drop editor",
+        "Step-by-step guidance",
+        "Pre-designed layout options",
+        "Domain names & professional email",
+        "Video backgrounds",
+        "Site stats",
+        "Create a blog"
+      ]
+    },
+    {
+      id: 2,
+      list: [    
+        "eCommerce",
+        "Unlimited products",
+        "Optimized one-page checkout",
+        "Automatic tax calculator",
+        "Real time shipping rates",
+        "Abandoned cart recovery",
+        "Coupon codes",
+        "Digital gift cards"
+      ]
+    },
+    {
+      id: 3,
+      list: [    
+        "Marketing",
+        "Fully integrated with site & store",
+        "Suggested emails created automatically",
+        "Email to sales insights",
+        "Branded billing emails",
+        "Powerful app integrations",
+        "SEO Guide & scorecard",
+        "Automation for marketing autopilot"
+      ]
+    }
+    ]
+  }
+];
 
 export default Data;
