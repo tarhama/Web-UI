d483c39e2d3866258fbb073c7710a94eb2d9283f
Matus <bernatmatus@gmail.com>
2017-06-12 09:56:25 +0200

    navbar, layer of section 9, update main css

--- src/components/navbar/navbarComponent.jsx
+++ src/components/navbar/navbarComponent.jsx
@@ -6,23 +6,42 @@ import NavbarRightComponent from './navbarRight/navbarRightComponent';
 import './navbarComponent.scss';
 
 class NavbarComponent extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     scrollDown: false,
-  //   };
-  //   this.setBackground = this.setBackground.bind(this);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      changeNavbar: true,
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
 
-  // setBackground() {
-  //   this.setState({
-  //     scrollDown: !this.state.scrollDown,
-  //   });
-  // }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    let scrollTop = event.srcElement.body.scrollTop;
+    let navbarHeight = 260;
+    console.log(scrollTop);
+    if(scrollTop > navbarHeight) {
+      console.log(1);
+      this.setState({
+        changeNavbar: false,
+      })
+    } else {
+      console.log(2);
+      this.setState({
+        changeNavbar: true,
+      })
+    }
+  }
+
+  isActive(){
+    return 'navbarContainer ' + (this.state.changeNavbar ? 'navbarTransparent' : 'navbarWhiteBackground');
+  }
 
   render() {
     return(
-      <div className="navbarTransparent navbarContainer">
+      <div className={this.isActive()}>
         <div className="navbarComponent">
           <nav>
             <NavbarLeftComponent />
