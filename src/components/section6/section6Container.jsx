e00ec5ba07c9e3a2de09c8b9a4b2b9b0dfdd5c3c
Matus <bernatmatus@gmail.com>
2017-06-14 15:59:07 +0200

    section6 init

---
+++ src/components/section6/section6Container.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TextContent from './textContent/textContent';
+import './section6Container.scss';
+
+class Section6Container extends Component {
+  render() {
+    return(
+      <div className="containerSec6">
+        <TextContent />
+      </div>
+    )
+  }
+}
+
+export default Section6Container;
---
+++ src/store/dataSection6.js
@@ -0,0 +1,17 @@
+
+export const DataSec6 = {
+    title: "We've got your back",
+    buttons: [
+      {
+        id: 1,
+        title: "get started",
+      },
+      {
+        id: 2,
+        title: "learn more",
+      } 
+    ],  
+    text: "Talk to our award-winning Success Team available to help you create " +
+          "a website and achieve your goals. Join the Weebly Community, a vibrant " +
+          "network of people doing what they love.",
+};
