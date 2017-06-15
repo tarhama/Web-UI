2c759fcad6be85d04b4254e3181aca3a1ab35596
Matus <bernatmatus@gmail.com>
2017-06-15 15:59:49 +0200

    init responsive navbar

--- src/components/navbar/navbarLeft/navbarLeftComponent.jsx
+++ src/components/navbar/navbarLeft/navbarLeftComponent.jsx
@@ -2,15 +2,50 @@ import React, { Component } from 'react';
 import "./navbarLeftComponent.scss";
 
 class NavbarLeftComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      changeIcon: true,
+    };
+    this.resizePage = this.resizePage.bind(this);
+  }
+
+  componentDidMount() {
+    this.resizePage();
+    window.addEventListener('resize', this.resizePage);
+  }
+
+  resizePage() {
+    let minWidth = 768;
+    let currentWidth = window.innerWidth;
+
+    if(currentWidth < minWidth) {
+      this.setState({
+        changeIcon: false,
+      });
+    } else {
+      this.setState({
+        changeIcon: true,
+      });
+    }
+  }
+
+  activeClass(){
+    return 'navbarContainer ' + (this.state.changeIcon ? 'navbarTransparent' : 'navIcon');
+  }
+
   render() {
     return(
       <div className="navLeftComponent">
-        <ul>
+        <ul style={{display: this.state.changeIcon ? 'flex' : 'none'}}>
           <li>features</li>
           <li>themes</li>
           <li>pricing</li>
           <li>more</li>
         </ul>
+        <div className="navIcon" style={{display: !this.state.changeIcon ? 'block' : 'none'}}>
+             ☰
+        </div>
       </div>
     )
   }
--- src/components/section6/section6Container.jsx
+++ src/components/section6/section6Container.jsx
@@ -5,11 +5,11 @@ import './section6Container.scss';
 
 class Section6Container extends Component {
   render() {
+    const videoLink = "//cdn2.editmysite.com/videos/landing-pages/global/home-weebly4/v2/cs/cs.mp4";
     return(
       <div className="containerSec6">
         <video className="videoSec6" ref={video => this.video = video} playsInline autoPlay muted loop>
-          <source src="//cdn2.editmysite.com/videos/landing-pages/global/home-weebly4/v2/cs/cs.mp4"
-                  type="video/mp4"/>
+          <source src={videoLink} type="video/mp4"/>
         </video>
         <div className="Container">
         <TextContent />
