9e79184eea2e52efb21bd07ee08dfd0aad9ec32e
lubos.jasko <jasko.lubos@gmail.com>
2017-06-16 10:00:20 +0200

    repaired renderImg

--- src/components/section3/parts/renderImages.jsx
+++ src/components/section3/parts/renderImages.jsx
@@ -1,28 +1,35 @@
-// import React, {Component} from 'react';
-// import './../section3_images';
-//
-// export default class RenderImages extends Component {
-//
-//     constructor(props) {
-//         super(props);
-//
-//
-//     }
-//
-//     renderImages(){
-//         <div className={"image-items image-transition "} style={dataImage.className}>
-//             <img src={this.props.src} alt={this.props.name}/>
-//         </div>
-//     }
-//
-//
-//     render() {
-//     return(
-//         <div>
-//             {this.props.dataImage.map(this.renderImages)}
-//     </div>
-//
-//         )}
-// }
-//
-//
+import React, {Component} from 'react';
+import './../section3_images';
+
+export default class RenderImages extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			imageProps: this.props.dataImage
+		};
+
+		this.renderImages = this.renderImages.bind(this);
+	}
+
+	renderImages(image) {
+		return (
+			<div className={"image-items image-transition " + image.className}>
+				<img src={image.src} alt={image.name}/>
+			</div>
+		)
+	}
+
+	render() {
+		return (
+			<div>
+				{this.state.imageProps.map(this.renderImages)}
+			</div>
+
+		)
+	}
+}
+
+
+
--- src/components/section3/section3_images.jsx
+++ src/components/section3/section3_images.jsx
@@ -2,39 +2,28 @@ import React, {Component} from 'react';
 import * as data from '../../store/dataSection3';
 import * as data1 from '../../store/dataSection4';
 import './section3_main.css';
+import RenderImage from './parts/renderImages';
 
 class Section3Images extends Component {
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.state = {
-            image: data.DataSect3.images,
-            image1: data1.DataSect4.images,
-            scrollTop: 0,
-        };
-    }
+		this.state = {
+			image: data.DataSect3.images,
+			image1: data1.DataSect4.images,
+			scrollTop: 0,
+		};
+	}
 
-    renderImages(image) {
-        return(
-
-                <div className={"image-items image-transition" + image.className}>
-                    <img src={image.src} alt={image.name}/>
-                </div>
-
-        )
-    }
-
-    render() {
-        const {image} = this.state;
-
-        return (
-            <div className="d-flex justify-content-center">
-                <div className="image-wrapper relative">
-            {image.map(this.renderImages)}
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="d-flex justify-content-center">
+				<div className="image-wrapper relative">
+					<RenderImage dataImage={this.state.image}/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export  default Section3Images;
