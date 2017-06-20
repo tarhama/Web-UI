a6d1fa1613457af578d625efb01ea3630d6f495b
Matus <bernatmatus@gmail.com>
2017-06-20 08:45:50 +0200

    repair section6 responsive, add left navbar dropdown

---
+++ src/store/dataNavbar.jsx
@@ -0,0 +1,22 @@
+export const DataNavbar = {
+  leftNavbar: [
+  {
+    title: 'product',
+  },
+  {
+    title: 'resources',
+  },
+  {
+    title: 'support',
+  },
+  {
+    title: 'partners',
+  },
+  {
+    title: 'company',
+  },
+  {
+    title: 'english',
+  }
+  ]
+};
